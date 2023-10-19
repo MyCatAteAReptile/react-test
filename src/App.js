140ed98c65711c0884af686f559ec26c337c846a
MyCatAteAReptile <114024484+MyCatAteAReptile@users.noreply.github.com>
2023-10-19 14:57:57 +0500

    1

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useRef, useState } from "react";
 import './styles/App.css';
 import PostList from "./components/PostList";
 import { PostForm } from "./components/PostForm";
+import { MySelect } from "./components/UI/select/MySelect";
 
 function App() {
   const [posts, setPosts] = useState([
@@ -9,6 +10,8 @@ function App() {
     {id: 2, title: 'Javascript 2', body: 'Description'},
     {id: 3, title: 'Javascript 3', body: 'Description'}
   ])
+
+  const [selectedSort, setSelectedSort] = useState('')
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
@@ -18,9 +21,25 @@ function App() {
     setPosts(posts.filter(p => p.id !== post.id))
   }
 
+  const sortPosts = (sort) => {
+    
+  }
+
   return (
     <div className="App">
       <PostForm create={createPost}/>
+      <hr style={{margin: '15px 0'}}/>
+      <div>
+        <MySelect
+          value={selectedSort}
+          onChange={sort => setSelectedSort(sort)}
+          defaultValue="Сортировка по"
+          options={[
+            {value: 'title', name: 'По названию'},
+            {value: 'body', name: 'По описанию'},
+          ]}
+        />
+      </div>
       {posts.length // условная отрисовка
           ? <PostList remove={removePost} posts={posts} title='Жиес'/>
           : 
